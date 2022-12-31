cd463e22b6c3774c3d561d0a46594df8fb3b7afc
Nicholas <avatar.nick@gmail.com>
2022-12-31 11:41:46 -0700

    Corrected errors on FrontEnd and improved CSS

---
+++ js/index2.js
@@ -0,0 +1,12 @@
+
+
+function verifyInputs() {
+
+    var name = documents.getElementById('name').value;
+    var email = documents.getElementById('email').value;
+    var password = documents.getElementById('password').value;
+    var passwordCheck = documents.getElementById('passwordCheck').value;
+    var message = document.getElementById('message');
+
+   
+}
